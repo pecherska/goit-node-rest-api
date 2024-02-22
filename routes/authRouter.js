984ea34d88bea26142ae473ce418e034bddacfd1
Nina Pecherska <npecherska@gmail.com>
2024-02-22 21:40:42 +0200

    initial commit

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -6,13 +6,21 @@ import {
   wrappedRegister,
   wrappedGetCurrent,
   wrappedLogout,
+  wrappedUpdateAvatar,
 } from "../controllers/authControllers.js";
 import authenticate from "../helpers/authenticate.js";
+import upload from "../helpers/upload.js";
 
 const authRouter = express.Router();
 authRouter.post("/register", validateBody(registerSchema), wrappedRegister);
 authRouter.post("/login", validateBody(loginSchema), wrappedLogin);
 authRouter.get("/current", authenticate, wrappedGetCurrent);
 authRouter.post("/logout", authenticate, wrappedLogout);
+authRouter.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  wrappedUpdateAvatar
+);
 
 export default authRouter;
